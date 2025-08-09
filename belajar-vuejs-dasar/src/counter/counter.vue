b41280dbe1c24ecae40c2d905d8ae3313b12c394
adptra01 <m.a.syaputra.94@gmail.com>
2025-08-10 02:13:32 +0700

    feat: Added basic counter application with Vue.js

--- belajar-vuejs-dasar/src/counter/counter.vue
+++ belajar-vuejs-dasar/src/counter/counter.vue
@@ -1,5 +1,8 @@
 <script setup>
+    import { ref } from "vue";
+
     let count = 0;
+    let count2 = ref(0);
 
     function increment() {
         count++;
@@ -10,6 +13,14 @@
         count--;
         document.getElementById("count-display").innerText = "Nilai Count: " + count;
     }
+
+    function increment2() {
+        count2.value++;
+    }
+
+    function decrement2() {
+        count2.value--;
+    }
 </script>
 
 <template>
@@ -18,6 +29,11 @@
         <button type="button" v-on:click="increment">Tambah</button>
         ||
         <button type="button" v-on:click="decrement">kurang</button>
+        <p>=========================</p>
+        <h1 id="count-display">Nilai Count : {{ count2 }}</h1>
+        <button type="button" v-on:click="increment2">Tambah</button>
+        ||
+        <button type="button" v-on:click="decrement2">kurang</button>
     </div>
 </template>
 
